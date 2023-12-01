18e34885809c904d4e3ca7920c3d8a21a6c7fc2b
bajak1994 <mateusz.bajak@airfleet.co>
2023-12-01 16:29:32 +0100

    feat(was-this-article-helpful-find-co): Volume set up for the plugin

---
+++ was-this-article-helpful-find-co/interview-task/was-this-article-helpful-find-co.php
@@ -0,0 +1,8 @@
+<?php
+/*
+Plugin Name: Was This Article Helpful? - Find.co
+Plugin URI: https://find.co/
+Description: A simple voting system for your articles.
+Version: 1.0
+Author: Mateusz Bajak
+*/
